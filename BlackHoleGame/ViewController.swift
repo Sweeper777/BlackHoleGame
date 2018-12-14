2bd5a866165826c26d06fa62e8d2b52791397814
Sweeper777 <sumulang@gmail.com>
2018-12-14 07:11:13 +0000

    Initial Commit

---
+++ BlackHoleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackHoleGame
+//
+//  Created by Mulang Su on 14/12/2018.
+//  Copyright © 2018 Mulang Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
