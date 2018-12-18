a35d3ea297e55af43910e5617e96e5c18cdd4c2f
Sweeper777 <sumulang@gmail.com>
2018-12-18 09:07:48 +0800

    implemented draw

--- BlackHoleGame/BoardView.swift
+++ BlackHoleGame/BoardView.swift
@@ -6,6 +6,25 @@ class BoardView: UIView {
             setNeedsDisplay()
         }
     }
+    
+    override func draw(_ rect: CGRect) {
+        if board == nil {
+            return
+        }
+        
+        let size = CGSize(width: bounds.width / CGFloat(board.rowCount), height: bounds.height / CGFloat(board.rowCount))
+        for row in 0..<board.rowCount {
+            for index in 0..<row {
+                let path = UIBezierPath(ovalIn: CGRect(origin: point(forCircleInRow: row, atIndex: index), size: size))
+                path.lineWidth = 3
+                UIColor.black.setStroke()
+                path.stroke()
+            }
+        }
+        
+        self.subviews.forEach { $0.removeFromSuperview() }
+    }
+    
     func point(forCircleInRow row: Int, atIndex index: Int) -> CGPoint {
         let n = CGFloat(board.rowCount)
         let c = CGFloat(board.rowCount - row - 1)
