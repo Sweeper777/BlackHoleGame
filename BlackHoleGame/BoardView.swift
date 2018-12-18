147ab4f2401bf3e359b84a7684287fa7b9d1e3e6
Sweeper777 <sumulang@gmail.com>
2018-12-18 10:29:43 +0800

    renamed point(forCircleInRow:atIndex:)

--- BlackHoleGame/BoardView.swift
+++ BlackHoleGame/BoardView.swift
@@ -45,11 +45,11 @@ class BoardView: UIView {
         self.subviews.forEach { $0.removeFromSuperview() }
     }
     
-    func point(forCircleInRow row: Int, atIndex index: Int) -> CGPoint {
+    func pointInBoardFrame(forCircleInRow row: Int, atIndex index: Int) -> CGPoint {
         let n = CGFloat(board.rowCount)
         let c = CGFloat(board.rowCount - row - 1)
-        let w = bounds.width
-        let h = bounds.height
+        let w = actualBoardFrame.width
+        let h = actualBoardFrame.height
         let x = (2 * w * CGFloat(index) + w * c) / (2 * n)
         let y = (n - c - 1) * h / n
         return CGPoint(x: x, y: y)
