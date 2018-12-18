90081cac628dfaa76d1084be74c66fa9db171057
Sweeper777 <sumulang@gmail.com>
2018-12-18 09:08:00 +0800

    added a BoardView in VC

--- BlackHoleGame/ViewController.swift
+++ BlackHoleGame/ViewController.swift
@@ -1,18 +1,14 @@
-//
-//  ViewController.swift
-//  BlackHoleGame
-//
-//  Created by Mulang Su on 14/12/2018.
-//  Copyright © 2018 Mulang Su. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let board = BoardView(frame: CGRect(x: 10, y: 10, width: 300, height: 300))
+        board.backgroundColor = .white
+        board.board = Game().board
+        view.addSubview(board)
     }
 
 
