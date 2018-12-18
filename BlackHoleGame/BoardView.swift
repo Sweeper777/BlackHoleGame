595b5943dd5c569518857aa7aad95839a6b399b3
Sweeper777 <sumulang@gmail.com>
2018-12-18 09:07:36 +0800

    added point(forCircleInRow:atIndex)

--- BlackHoleGame/BoardView.swift
+++ BlackHoleGame/BoardView.swift
@@ -6,4 +6,13 @@ class BoardView: UIView {
             setNeedsDisplay()
         }
     }
+    func point(forCircleInRow row: Int, atIndex index: Int) -> CGPoint {
+        let n = CGFloat(board.rowCount)
+        let c = CGFloat(board.rowCount - row - 1)
+        let w = bounds.width
+        let h = bounds.height
+        let x = (2 * w * CGFloat(index) + w * c) / (2 * n)
+        let y = (n - c - 1) * h / n
+        return CGPoint(x: x, y: y)
+    }
 }
