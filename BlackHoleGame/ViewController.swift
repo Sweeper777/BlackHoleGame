2c31bb0c38c36c78e76524169c915c03aed335a6
Sweeper777 <sumulang@gmail.com>
2018-12-19 09:15:43 +0800

    implemented BoardViewDelegate in VC

--- BlackHoleGame/ViewController.swift
+++ BlackHoleGame/ViewController.swift
@@ -1,16 +1,19 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BoardViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let board = BoardView(frame: CGRect(x: 10, y: 10, width: 300, height: 300))
+        let board = BoardView(frame: CGRect(x: 10, y: 10, width: 300, height: 500))
         board.backgroundColor = .white
         board.board = Game().board
+        board.delegate = self
         view.addSubview(board)
     }
-
-
+    
+    func didTouchCircle(inRow row: Int, atIndex index: Int) {
+        print("Touched: row \(row), index \(index)")
+    }
 }
 
