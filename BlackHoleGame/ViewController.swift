b512c64399576cb64cbf8f4c7be3912261af0a4f
Sweeper777 <sumulang@gmail.com>
2018-12-22 10:09:32 +0800

    implemenetd AI

--- BlackHoleGame/ViewController.swift
+++ BlackHoleGame/ViewController.swift
@@ -4,6 +4,7 @@ class ViewController: UIViewController, BoardViewDelegate {
 
     var board: BoardView!
     let game = Game()
+    var turn = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +33,16 @@ class ViewController: UIViewController, BoardViewDelegate {
         
         game.makeMove(row: row, index: index)
         board.board = game.board
+        turn += 1
+        DispatchQueue.main.async {
+            [weak self] in
+            guard let `self` = self else { return }
+            let ai = GameAI(game: self.game, myColor: self.game.currentTurn)
+            let move = ai.getNextMove(searchDepth: self.searchDepth(forTurn: self.turn))
+            self.game.makeMove(row: move.row, index: move.index)
+            self.board.board = self.game.board
+            self.turn += 1
+        }
     }
 }
 
