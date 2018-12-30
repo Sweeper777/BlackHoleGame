8c8f520e973e1e87f7660a884fb4ef2c1c8cf0dc
Sweeper777 <sumulang@gmail.com>
2018-12-30 12:31:07 +0800

    added sucked in anmation

--- BlackHoleGame/CircleView.swift
+++ BlackHoleGame/CircleView.swift
@@ -35,4 +35,11 @@ extension CircleView {
             .then(animation: scaled(duration: 0.3, x: 1.2, y: 1.2))
             .then(animation: scaled(duration: 0.1, x: 1, y: 1))
     }
+    
+    func suckedIn(x: CGFloat, y: CGFloat) -> Animate {
+        return Animate()
+            .then(animation: translated(duration: 1, x: x - self.x, y: y - self.y, options: [.curveEaseIn]))
+            .and(animation: scaled(duration: 1, x: 0, y: 0, options: [.curveEaseIn]))
+            .do(block: removeFromSuperview)
+    }
 }
