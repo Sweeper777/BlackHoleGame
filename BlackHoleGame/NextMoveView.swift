e695ce842c3746914c259c87f0a7b2dd92757b69
Sweeper777 <sumulang@gmail.com>
2019-01-01 11:42:18 +0800

    added number and color properties

--- BlackHoleGame/NextMoveView.swift
+++ BlackHoleGame/NextMoveView.swift
@@ -3,4 +3,16 @@ import UIKit
 class NextMoveView : UIView {
     @IBOutlet var circleView: CircleView!
     
+    var number: Int? {
+        didSet {
+            circleView.number = number
+        }
+    }
+    
+    var color: UIColor = .clear {
+        didSet {
+            circleView.backgroundColor = color
+        }
+    }
+    
 }
