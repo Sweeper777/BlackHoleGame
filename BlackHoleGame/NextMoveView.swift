3b7297bd81f9de22ab6a69c44888d6e71c9bf1d6
Sweeper777 <sumulang@gmail.com>
2019-01-02 09:15:27 +0800

    added initialisers

--- BlackHoleGame/NextMoveView.swift
+++ BlackHoleGame/NextMoveView.swift
@@ -35,4 +35,13 @@ class NextMoveView : UIView {
         return view
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
 }
