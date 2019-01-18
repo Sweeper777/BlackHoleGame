fdba4365a8dea901e8822a7dd0f4bfbac9212f13
Sweeper777 <sumulang@gmail.com>
2019-01-18 06:50:12 +0000

    added constraints and set properties of play button

--- BlackHoleGame/MainMenuViewController.swift
+++ BlackHoleGame/MainMenuViewController.swift
@@ -7,4 +7,17 @@ class MainMenuViewController: UIViewController {
     
     var playButton: PressableButton!
     
+    override func viewDidLoad() {
+        playButton = PressableButton(frame: .zero)
+        buttonContainer.addSubview(playButton)
+        playButton.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(8)
+            make.centerX.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(4).offset(-8)
+            make.width.lessThanOrEqualTo(playButton.snp.height).multipliedBy(5)
+            make.left.equalToSuperview().offset(8).priority(.high)
+            make.right.equalToSuperview().offset(-8).priority(.high)
+        }
+        playButton.setTitle("PLAY", for: .normal)
+    }
 }
