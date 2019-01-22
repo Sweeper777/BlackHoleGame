1ed7fc6713651dbdfd0d748087b394c05bf15941
Sweeper777 <sumulang@gmail.com>
2019-01-22 06:32:02 +0000

    added view properties

--- BlackHoleGame/MapSelectorViewController.swift
+++ BlackHoleGame/MapSelectorViewController.swift
@@ -3,4 +3,10 @@ import SnapKit
 import SwiftyButton
 
 class MapSelectorViewController : UIViewController {
+    var mapContainer: UIView!
+    var map1Button: FlatButton!
+    var map2Button: FlatButton!
+    var map3Button: FlatButton!
+    var backButton: PressableButton!
+    
 }
