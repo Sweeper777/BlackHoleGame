4cc83951dc1797a8c410fd0583d9664d2a7657f7
Sweeper777 <sumulang@gmail.com>
2019-01-22 06:32:41 +0000

    added constraints for mapContainer

--- BlackHoleGame/MapSelectorViewController.swift
+++ BlackHoleGame/MapSelectorViewController.swift
@@ -9,4 +9,22 @@ class MapSelectorViewController : UIViewController {
     var map3Button: FlatButton!
     var backButton: PressableButton!
     
+    override func viewDidLoad() {
+        mapContainer = UIView(frame: .zero)
+        view.addSubview(mapContainer)
+        mapContainer.backgroundColor = .black
+        mapContainer.snp.makeConstraints { (make) in
+            make.height.equalTo(mapContainer.snp.width)
+            make.left.greaterThanOrEqualToSuperview().offset(8)
+            make.right.lessThanOrEqualToSuperview().offset(-8)
+            make.top.greaterThanOrEqualToSuperview().offset(8)
+            make.bottom.lessThanOrEqualToSuperview().offset(-8)
+            make.left.equalToSuperview().offset(8).priority(.high)
+            make.right.equalToSuperview().offset(-8).priority(.high)
+            make.top.equalToSuperview().offset(8).priority(.high)
+            make.bottom.equalToSuperview().offset(-8).priority(.high)
+            make.center.equalToSuperview()
+        }
+        
+    }
 }
